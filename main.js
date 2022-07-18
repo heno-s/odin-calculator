b466fe2732e242308372d34823a66f0a0d069793
janko12 <henrich.sury97@gmail.com>
2022-07-19 00:55:30 +0200

    Complete basic math functions

---
+++ main.js
@@ -0,0 +1,42 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function operate(operator, num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "UNKNOWN OPERATOR";
+            break;
+    }
+
+    return result;
+}
+
+console.log(operate("-", "3", "5"));
